e8efb4cfb4f2478a7e5df37525d92b75b087b310
Shashank Gupta <shashankgupta@MacBookAir.lan>
2026-02-07 15:48:43 -0500

    Multiple Seasons and csv files

--- multiple_csv.py
+++ multiple_csv.py
@@ -6,13 +6,20 @@ import pandas as pd
 # https://www.football-data.co.uk/mmz4281/2526/E3.csv
 # https://www.football-data.co.uk/mmz4281/2526/EC.csv
 
+
+
 root = 'https://www.football-data.co.uk/mmz4281/'
 leagues = ['E0', 'E1', 'E2', 'E3', 'EC']
 frames = []
 
 for league in leagues:
-     df = pd.read_csv(f"{root}2526/{league}.csv", encoding='unicode_escape')
-     frames.append(df)
+    for season in range(20, 25):
+        season_name = str(season) + str(season + 1)
+        df = pd.read_csv(f"{root}{season_name}/{league}.csv", encoding='unicode_escape')
+        df.insert(1, 'season', season)
+        frames.append(df)
+
 
 print(len(frames))
-print(frames[4])
+print(frames[24])
+
